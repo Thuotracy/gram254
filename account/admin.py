dd60c1656d8880f3c0a01282be7549bce42a58d8
tracy <tracyjacobs775@gmail.com>
2022-04-04 18:57:19 +0300

    start project

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+# from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from account.models import NUser, Profile, Image
 
